c9b0d3b48fcf671db7537eb3eaebf7e85c36472b
Jonathan Lange <jml@canonical.com>
2008-06-01 15:47:15 +1000

    setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+"""Distutils installer for pyunit3k."""
+
+from distutils.core import setup
+
+setup(name='pyunit3k',
+      author='Jonathan M. Lange',
+      author_email='jml+pyunit3k@mumak.net',
+      url='https://launchpad.net/pyunit3k',
+      description=('Extensions to the Python standard library unit testing '
+                   'framework'),
+      version='0.0.1',
+      packages=['pyunit3k'])
