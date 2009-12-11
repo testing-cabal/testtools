274ea9356128ba1d79cac4335e29903d2fcfb547
Robert Collins <robertc@robertcollins.net>
2009-12-11 15:53:12 +1100

    Make overwriting tarballs a little harder to do and expose a version number to clients.

--- setup.py
+++ setup.py
@@ -2,6 +2,18 @@
 """Distutils installer for testtools."""
 
 from distutils.core import setup
+import testtools
+version = '.'.join(str(component) for component in testtools.__version__[0:3])
+phase = testtools.__version__[3]
+if phase != 'final':
+    import bzrlib.workingtree
+    t = bzrlib.workingtree.WorkingTree.open_containing(__file__)[0]
+    if phase == 'alpha':
+        # No idea what the next version will be
+        version = 'next-%s' % t.branch.revno()
+    else:
+        # Preserve the version number but give it a revno prefix
+        version = version + '~%s' % t.branch.revno()
 
 setup(name='testtools',
       author='Jonathan M. Lange',
@@ -9,5 +21,5 @@ setup(name='testtools',
       url='https://launchpad.net/testtools',
       description=('Extensions to the Python standard library unit testing '
                    'framework'),
-      version='0.9.1',
+      version=version,
       packages=['testtools'])
--- testtools/__init__.py
+++ testtools/__init__.py
@@ -42,3 +42,17 @@ from testtools.testsuite import (
     ConcurrentTestSuite,
     )
 from testtools.utils import iterate_tests
+
+# same format as sys.version_info: "A tuple containing the five components of
+# the version number: major, minor, micro, releaselevel, and serial. All
+# values except releaselevel are integers; the release level is 'alpha',
+# 'beta', 'candidate', or 'final'. The version_info value corresponding to the
+# Python version 2.0 is (2, 0, 0, 'final', 0)."  Additionally we use a
+# releaselevel of 'dev' for unreleased under-development code.
+#
+# If the releaselevel is 'alpha' then the major/minor/micro components are not
+# established at this point, and setup.py will use a version of next-$(revno).
+# If the releaselevel is 'final', then the tarball will be major.minor.micro.
+# Otherwise it is major.minor.micro~$(revno).
+
+__version__ = (0, 9, 2, 'alpha', 0)
