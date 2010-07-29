45c5d80255ef3b98ef1ae4613ff0e38fb21665d0
Jonathan Lange <jml@canonical.com>
2010-07-29 19:48:37 +0100

    Release testtools 0.9.5

--- testtools/__init__.py
+++ testtools/__init__.py
@@ -55,4 +55,4 @@ from testtools.testsuite import (
 # If the releaselevel is 'final', then the tarball will be major.minor.micro.
 # Otherwise it is major.minor.micro~$(revno).
 
-__version__ = (0, 9, 4, 'final', 0)
+__version__ = (0, 9, 5, 'final', 0)
