c3eba46699b0758c59d18351989cf091347314b5
Jonathan Lange <jml@canonical.com>
2010-10-24 09:44:45 +0100

    Only run the Twisted-depending tests unconditionally.

--- testtools/tests/__init__.py
+++ testtools/tests/__init__.py
@@ -3,36 +3,43 @@
 # See README for copyright and licensing details.
 
 import unittest
-from testtools.tests import (
-    test_compat,
-    test_content,
-    test_content_type,
-    test_deferredruntest,
-    test_matchers,
-    test_monkey,
-    test_runtest,
-    test_spinner,
-    test_testtools,
-    test_testresult,
-    test_testsuite,
-    )
 
 
 def test_suite():
+    from testtools.tests import (
+        test_compat,
+        test_content,
+        test_content_type,
+        test_matchers,
+        test_monkey,
+        test_runtest,
+        test_testtools,
+        test_testresult,
+        test_testsuite,
+        )
     suites = []
     modules = [
         test_compat,
         test_content,
         test_content_type,
-        test_deferredruntest,
         test_matchers,
         test_monkey,
         test_runtest,
-        test_spinner,
         test_testresult,
         test_testsuite,
         test_testtools,
         ]
+    try:
+        # Tests that rely on Twisted.
+        from testtools.tests import (
+            test_deferredruntest,
+            test_spinner,
+            )
+    except ImportError:
+        pass
+    else:
+        modules.extend([test_deferredruntest, test_spinner])
+
     for module in modules:
         suites.append(getattr(module, 'test_suite')())
     return unittest.TestSuite(suites)
