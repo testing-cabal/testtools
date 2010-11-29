e2e918499cb4aa70fb21b27ba28f8a8635e78fdc
Robert Collins <robertc@robertcollins.net>
2010-11-30 11:45:14 +1300

    Refactor for reusability.

--- testtools/tests/test_run.py
+++ testtools/tests/test_run.py
@@ -11,12 +11,12 @@ import testtools
 from testtools import TestCase, run
 
 
-class TestRun(TestCase):
+if fixtures:
+    class SampleTestFixture(fixtures.Fixture):
+        """Creates testtools.runexample temporarily."""
 
-    def test_run_list(self):
-        if fixtures is None:
-            self.skipTest("Need fixtures")
-        package = self.useFixture(fixtures.PythonPackage(
+        def __init__(self):
+            self.package = fixtures.PythonPackage(
             'runexample', [('__init__.py', """
 from testtools import TestCase
 
@@ -28,9 +28,21 @@ class TestFoo(TestCase):
 def test_suite():
     from unittest import TestLoader
     return TestLoader().loadTestsFromName(__name__)
-""")]))
-        testtools.__path__.append(package.base)
-        self.addCleanup(testtools.__path__.remove, package.base)
+""")])
+
+        def setUp(self):
+            super(SampleTestFixture, self).setUp()
+            self.useFixture(self.package)
+            testtools.__path__.append(self.package.base)
+            self.addCleanup(testtools.__path__.remove, self.package.base)
+
+
+class TestRun(TestCase):
+
+    def test_run_list(self):
+        if fixtures is None:
+            self.skipTest("Need fixtures")
+        package = self.useFixture(SampleTestFixture())
         out = StringIO.StringIO()
         run.main(['-l', 'testtools.runexample.test_suite'], out)
         self.assertEqual("""testtools.runexample.TestFoo.test_bar
