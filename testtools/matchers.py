3a4cf1392597d34231e7dc0d39e78a0dffd5bc90
Jonathan Lange <jml@canonical.com>
2011-04-01 15:03:45 +0100

    Docs!

--- testtools/matchers.py
+++ testtools/matchers.py
@@ -118,8 +118,18 @@ class Mismatch(object):
 
 
 class MismatchDecorator(object):
+    """Decorate a ``Mismatch``.
+
+    Forwards all messages to the original mismatch object.  Probably the best
+    way to use this is inherit from this class and then provide your own
+    custom decoration logic.
+    """
 
     def __init__(self, original):
+        """Construct a `MismatchDecorator`.
+
+        :param original: A `Mismatch` object to decorate.
+        """
         self.original = original
 
     def describe(self):
