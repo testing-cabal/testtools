62db425cf32251a0780b3c694cc46b9544b5fd80
Jonathan Lange <jml@canonical.com>
2011-04-08 12:46:20 +0100

    Correct version.

--- testtools/__init__.py
+++ testtools/__init__.py
@@ -77,4 +77,4 @@ from testtools.distutilscmd import (
 # If the releaselevel is 'final', then the tarball will be major.minor.micro.
 # Otherwise it is major.minor.micro~$(revno).
 
-__version__ = (0, 9, 9, 'dev', 0)
+__version__ = (0, 9, 9, 'final', 0)
