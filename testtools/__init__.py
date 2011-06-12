cc600649429a9b362e50a0de5e749bb7ed8841ee
Robert Collins <robertc@robertcollins.net>
2011-06-12 12:37:20 +1200

    release 0.9.11

--- testtools/__init__.py
+++ testtools/__init__.py
@@ -77,4 +77,4 @@ from testtools.distutilscmd import (
 # If the releaselevel is 'final', then the tarball will be major.minor.micro.
 # Otherwise it is major.minor.micro~$(revno).
 
-__version__ = (0, 9, 11, 'dev', 0)
+__version__ = (0, 9, 11, 'final', 0)
