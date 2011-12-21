00b639936ae62c895dc660c976a0d40bb897896f
Martin <gzlist@googlemail.com>
2011-12-21 01:16:08 +0000

    Close files properly in test_from_stream_eager_loading

--- testtools/tests/test_content.py
+++ testtools/tests/test_content.py
@@ -130,11 +130,12 @@ class TestContent(TestCase):
     def test_from_stream_eager_loading(self):
         fd, path = tempfile.mkstemp()
         self.addCleanup(os.remove, path)
+        self.addCleanup(os.close, fd)
         os.write(fd, _b('some data'))
         stream = open(path, 'rb')
+        self.addCleanup(stream.close)
         content = content_from_stream(stream, UTF8_TEXT, buffer_now=True)
         os.write(fd, _b('more data'))
-        os.close(fd)
         self.assertThat(
             ''.join(content.iter_text()), Equals('some data'))
 
