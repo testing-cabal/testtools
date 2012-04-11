195aeb39f85d8b412434e59390087cfd349a22f8
Jonathan Lange <jml@mumak.net>
2012-04-11 13:04:44 +0100

    Documentation.

--- testtools/tags.py
+++ testtools/tags.py
@@ -4,19 +4,35 @@
 
 
 class TagContext(object):
+    """A tag context."""
 
     def __init__(self, parent=None):
+        """Create a new TagContext.
+
+        :parent: If provided, uses this as the parent context.  Any tags that
+            are current on the parent at the time of construction are current
+            in this context.
+        """
         self._parent = parent
         self._tags = set()
         if parent:
             self._tags.update(parent.get_current_tags())
 
     def get_current_tags(self):
+        """Return any current tags."""
         return set(self._tags)
 
     def get_parent(self):
+        """Return the parent.  If no parent, return None."""
         return self._parent
 
     def change_tags(self, new_tags, gone_tags):
+        """Change the tags on this context.
+
+        :param new_tags: A set of tags to add to this context.
+        :param gone_tags: A set of tags to remove from this context.
+        :return: The tags now current on this context.
+        """
         self._tags.update(new_tags)
         self._tags.difference_update(gone_tags)
+        return self.get_current_tags()
