04a65599fc80c5e57e5420e3db18acf469c16296
Jonathan Lange <jml@mumak.net>
2012-04-11 12:40:59 +0100

    A class-based approach.

--- testtools/tags.py
+++ testtools/tags.py
@@ -3,6 +3,25 @@
 """Tag support."""
 
 
+class TagContext(object):
+
+    def __init__(self, parent=None):
+        self._parent = parent
+        self._tags = set()
+        if parent:
+            self._tags.update(parent.get_current_tags())
+
+    def get_current_tags(self):
+        return set(self._tags)
+
+    def get_parent(self):
+        return self._parent
+
+    def change_tags(self, new_tags, gone_tags):
+        self._tags.update(new_tags)
+        self._tags.difference_update(gone_tags)
+
+
 def new_tag_context(parent=None):
     if parent is None:
         return [set()]
--- testtools/tests/test_tags.py
+++ testtools/tests/test_tags.py
@@ -78,6 +78,25 @@ class TestTags(TestCase):
         self.assertEqual(parent, child_parent)
 
 
+class TestTagContext(TestTags):
+
+    def new_tag_context(self):
+        return tags.TagContext()
+
+    def change_tags(self, tag_context, new_tags, gone_tags):
+        tag_context.change_tags(new_tags, gone_tags)
+        return tag_context
+
+    def get_current_tags(self, tag_context):
+        return tag_context.get_current_tags()
+
+    def push_tag_context(self, parent):
+        return tags.TagContext(parent)
+
+    def pop_tag_context(self, child):
+        return child.get_parent()
+
+
 def test_suite():
     from unittest import TestLoader
     return TestLoader().loadTestsFromName(__name__)
