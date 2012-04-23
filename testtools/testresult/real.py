9b01a641947ced241007bbc61dd72902836891fb
Benji York <benji.york@canonical.com>
2012-04-23 14:56:40 -0400

    use a better name

--- testtools/testresult/real.py
+++ testtools/testresult/real.py
@@ -483,8 +483,8 @@ class ThreadsafeForwardingResult(TestResult):
                 self._global_tags, (new_tags, gone_tags))
 
 
-def _merge_tags(existing, tags):
-    new_tags, gone_tags = tags
+def _merge_tags(existing, changed):
+    new_tags, gone_tags = changed
     result_new = set(existing[0])
     result_gone = set(existing[1])
     result_new.update(new_tags)
