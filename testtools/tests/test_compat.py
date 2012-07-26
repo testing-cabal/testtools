a2538440a88396671cfd5f2d2f87f27c06055650
Martin Packman <martin.packman@canonical.com>
2012-07-26 12:26:14 +0100

    Only assert that a reraised traceback has the original as a tail

--- testtools/tests/test_compat.py
+++ testtools/tests/test_compat.py
@@ -395,7 +395,7 @@ class TestReraise(testtools.TestCase):
     """Tests for trivial reraise wrapper needed for Python 2/3 changes"""
 
     def test_exc_info(self):
-        """After reraise exc_info matches bar one extra traceback level"""
+        """After reraise exc_info matches plus some extra traceback"""
         try:
             raise ValueError("Bad value")
         except ValueError:
@@ -406,8 +406,9 @@ class TestReraise(testtools.TestCase):
             _new_exc_info = sys.exc_info()
         self.assertIs(_exc_info[0], _new_exc_info[0])
         self.assertIs(_exc_info[1], _new_exc_info[1])
-        self.assertEqual(traceback.extract_tb(_exc_info[2]),
-            traceback.extract_tb(_new_exc_info[2])[1:])
+        expected_tb = traceback.extract_tb(_exc_info[2])
+        self.assertEqual(expected_tb,
+            traceback.extract_tb(_new_exc_info[2])[-len(expected_tb):])
 
     def test_custom_exception_no_args(self):
         """Reraising does not require args attribute to contain params"""
