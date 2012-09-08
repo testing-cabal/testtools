ae16a490538daae6b3da7b65017b4239a2e1fdee
Jonathan Lange <jml@canonical.com>
2012-09-08 17:11:51 +0100

    Move the tests.

--- testtools/tests/matchers/__init__.py
+++ testtools/tests/matchers/__init__.py
@@ -6,9 +6,11 @@ from unittest import TestSuite
 
 def test_suite():
     from testtools.tests.matchers import (
+        test_basic,
         test_core,
         )
     modules = [
+        test_basic,
         test_core,
         ]
     suites = map(lambda x: x.test_suite(), modules)
