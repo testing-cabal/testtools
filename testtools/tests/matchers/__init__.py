a7a99a85a28a35d2584e9934b192f7ba5ca3a19f
Jonathan Lange <jml@canonical.com>
2012-09-08 18:08:15 +0100

    Split up the tests.

--- testtools/tests/matchers/__init__.py
+++ testtools/tests/matchers/__init__.py
@@ -8,12 +8,22 @@ def test_suite():
     from testtools.tests.matchers import (
         test_basic,
         test_core,
+        test_datastructures,
+        test_dict,
+        test_doctest,
+        test_exception,
         test_filesystem,
+        test_higherorder,
         )
     modules = [
         test_basic,
         test_core,
+        test_datastructures,
+        test_dict,
+        test_doctest,
+        test_exception,
         test_filesystem,
+        test_higherorder,
         ]
     suites = map(lambda x: x.test_suite(), modules)
     return TestSuite(suites)
