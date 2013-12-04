fed3c1181508c1b8f434c574209b67b7c54d9c74
Daniel Watkins <daniel@daniel-watkins.co.uk>
2013-12-04 16:22:05 +0000

    Split TracebackContent in to clearer methods.

--- testtools/content.py
+++ testtools/content.py
@@ -186,13 +186,25 @@ class TracebackContent(StackLinesContent):
     def __init__(self, err, test):
         if err is None:
             raise ValueError("err may not be None")
+        self.err = err
 
-        exctype, value, tb = err
+        prefix = _TB_HEADER
+        stack_lines = self._get_stack_lines()
+        postfix = self._get_postfix()
+
+        return super(TracebackContent, self).__init__(stack_lines,
+                                                      prefix,
+                                                      postfix)
+
+    def _get_stack_lines(self):
+        _, _, tb = self.err
         # Skip test runner traceback levels
         if StackLinesContent.HIDE_INTERNAL_STACK:
             while tb and '__unittest' in tb.tb_frame.f_globals:
                 tb = tb.tb_next
+        return traceback.extract_tb(tb)
 
+    def _get_postfix(self):
         # testtools customization. When str is unicode (e.g. IronPython,
         # Python 3), traceback.format_exception_only returns unicode. For
         # Python 2, it returns bytes. We need to guarantee unicode.
@@ -200,14 +212,8 @@ class TracebackContent(StackLinesContent):
             format_exception_only = traceback.format_exception_only
         else:
             format_exception_only = _format_exception_only
-
-        prefix = _TB_HEADER
-        stack_lines = traceback.extract_tb(tb)
-        postfix = ''.join(format_exception_only(exctype, value))
-
-        return super(TracebackContent, self).__init__(stack_lines,
-                                                      prefix,
-                                                      postfix)
+        exctype, value, _ = self.err
+        return ''.join(format_exception_only(exctype, value))
 
 
 class StacktraceContent(StackLinesContent):
