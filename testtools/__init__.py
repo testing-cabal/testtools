39414addc8753620b2255a015d44a8d6a4003e99
Thomi Richards <thomi.richards@canonical.com>
2014-08-29 13:04:48 +1200

    Opening 0.9.39 for development.

--- testtools/__init__.py
+++ testtools/__init__.py
@@ -122,4 +122,4 @@ else:
 # If the releaselevel is 'final', then the tarball will be major.minor.micro.
 # Otherwise it is major.minor.micro~$(revno).
 
-__version__ = (0, 9, 38, 'final', 0)
+__version__ = (0, 9, 39, 'dev', 0)
