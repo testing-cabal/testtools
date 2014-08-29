718ced2229a20925c82cbc198e8cbaf9f1b39dd9
Robert Collins <robertc@robertcollins.net>
2014-08-29 13:42:57 +1200

    Opening 0.9.40 for development.
    Change-Id: I992a7f86c2edd2bf65bdfc89dbd6cd2689e96fb5

--- testtools/__init__.py
+++ testtools/__init__.py
@@ -122,4 +122,4 @@ else:
 # If the releaselevel is 'final', then the tarball will be major.minor.micro.
 # Otherwise it is major.minor.micro~$(revno).
 
-__version__ = (0, 9, 39, 'final', 0)
+__version__ = (0, 9, 40, 'dev', 0)
