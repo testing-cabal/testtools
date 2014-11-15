e7e5d75697ec17d54c0847846c9048121eac9e00
Robert Collins <robertc@robertcollins.net>
2014-11-15 23:00:58 +1300

    Opening 1.2.1 for development.
    Change-Id: I15e818502cd937d509dc73578b206183ee68f749

--- testtools/__init__.py
+++ testtools/__init__.py
@@ -122,4 +122,4 @@ else:
 # If the releaselevel is 'final', then the tarball will be major.minor.micro.
 # Otherwise it is major.minor.micro~$(revno).
 
-__version__ = (1, 2, 0, 'final', 0)
+__version__ = (1, 2, 1, 'dev', 0)
