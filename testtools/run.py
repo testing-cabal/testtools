a0e61b3a2061038431b2e872390ce1b87b59e6f4
Robert Collins <robertc@robertcollins.net>
2015-03-10 13:18:49 +1300

    Bug #1430076 fix installing when extras is not installed.
    Change-Id: Icc9c40d6ef74e6b12323fdab7ce0581ec68794b7

--- testtools/run.py
+++ testtools/run.py
@@ -25,7 +25,10 @@ from testtools.testsuite import filter_by_ids, iterate_tests, sorted_tests
 defaultTestLoader = unittest.defaultTestLoader
 defaultTestLoaderCls = unittest.TestLoader
 have_discover = True
-discover_impl = unittest.loader
+# This shouldn't really be public - its legacy.  Try to set it if we can, and
+# if we can't (during installs before unittest2 is installed) just stub it out
+# to None.
+discover_impl = getattr(unittest, 'loader', None)
 
 # Kept for API compatibility, but no longer used.
 BUFFEROUTPUT = ""
