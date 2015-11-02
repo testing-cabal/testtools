fdc31f032d5fa135a8ba43486c1d9b1538072688
Jonathan Lange <jml@mumak.net>
2015-11-02 19:59:04 +0000

    Extract got_file

--- testtools/testresult/real.py
+++ testtools/testresult/real.py
@@ -689,6 +689,24 @@ class TestRecord(PRecord):
         """
         return self.set(timestamps=(self.timestamps[0], timestamp))
 
+    def got_file(self, file_name, file_bytes, mime_type=None):
+        """Called when we receive file information.
+
+        ``mime_type`` is only used when this is the first time we've seen data
+        from this file.
+        """
+        if file_name in self.details:
+            case = self
+        else:
+            content_type = _make_content_type(mime_type)
+            content_bytes = []
+            case = self.transform(
+                ['details', file_name],
+                Content(content_type, lambda: content_bytes))
+
+        case.details[file_name].iter_bytes().append(file_bytes)
+        return case
+
 
 class StreamToDict(StreamResult):
     """A specialised StreamResult that emits a callback as tests complete.
@@ -759,14 +777,7 @@ class StreamToDict(StreamResult):
         case = case.got_timestamp(timestamp)
 
         if file_name is not None:
-            if file_name not in case.details:
-                content_type = _make_content_type(mime_type)
-                content_bytes = []
-                case = case.transform(
-                    ['details', file_name],
-                    Content(content_type, lambda: content_bytes))
-
-            case.details[file_name].iter_bytes().append(file_bytes)
+            case = case.got_file(file_name, file_bytes, mime_type)
 
         if test_tags is not None:
             case = case.set('tags', test_tags)
