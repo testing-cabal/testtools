03061713d44cd13a10aa0ebe4ba0a45a3a6b58e5
Jonathan Lange <jml@mumak.net>
2015-12-04 18:12:35 +0000

    Get pre_set_up & post_tear_down from scenario

--- testtools/tests/samplecases.py
+++ testtools/tests/samplecases.py
@@ -138,12 +138,16 @@ def _make_behavior_scenarios(stage):
 
 def make_case_for_behavior_scenario(case):
     """Given a test with a behavior scenario installed, make a TestCase."""
+    cleanup_behavior = getattr(case, 'cleanup_behavior', None)
+    cleanups = [cleanup_behavior] if cleanup_behavior else []
     return make_test_case(
         case.getUniqueString(),
-        set_up=case.set_up_behavior,
-        test_body=case.body_behavior,
-        tear_down=case.tear_down_behavior,
-        cleanups=(case.cleanup_behavior,),
+        set_up=getattr(case, 'set_up_behavior', _do_nothing),
+        test_body=getattr(case, 'body_behavior', _do_nothing),
+        tear_down=getattr(case, 'tear_down_behavior', _do_nothing),
+        cleanups=cleanups,
+        pre_set_up=getattr(case, 'pre_set_up_behavior', _do_nothing),
+        post_tear_down=getattr(case, 'post_tear_down_behavior', _do_nothing),
     )
 
 
