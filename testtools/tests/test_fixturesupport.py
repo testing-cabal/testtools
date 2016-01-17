96ee770f1f6b892a6b85bcb475ef232e24027681
Julian Edwards <bigjools@gmail.com>
2016-01-17 21:25:35 +1000

    Grab details from MultipleExceptions in fixture.setUp()

--- testtools/tests/test_fixturesupport.py
+++ testtools/tests/test_fixturesupport.py
@@ -10,14 +10,16 @@ from testtools import (
     content_type,
     )
 from testtools.compat import _b, _u
-from testtools.matchers import Contains
+from testtools.matchers import (
+    Contains,
+    Equals,
+    )
 from testtools.testresult.doubles import (
     ExtendedTestResult,
     )
 
 fixtures = try_import('fixtures')
 LoggingFixture = try_import('fixtures.tests.helpers.LoggingFixture')
-
 
 class TestFixtureSupport(TestCase):
 
@@ -120,21 +122,30 @@ class TestFixtureSupport(TestCase):
         class BrokenFixture(fixtures.Fixture):
             def _setUp(self):
                 fixtures.Fixture._setUp(self)
-                self.addDetail('content', content.text_content("foobar"))
+                self.addDetail('broken', content.text_content("foobar"))
                 raise Exception("_setUp broke")
         fixture = BrokenFixture()
         class SimpleTest(TestCase):
             def test_foo(self):
+                self.addDetail('foo_content', content.text_content("foo ok"))
                 self.useFixture(fixture)
         result = ExtendedTestResult()
         SimpleTest('test_foo').run(result)
         self.assertEqual('addError', result._events[-2][0])
         details = result._events[-2][2]
-        self.assertEqual(['traceback', 'traceback-1'], sorted(details))
-        self.assertThat(
+        self.assertEqual(
+            ['broken', 'foo_content', 'traceback', 'traceback-1'],
+            sorted(details))
+        self.expectThat(
+            ''.join(details['broken'].iter_text()),
+            Equals('foobar'))
+        self.expectThat(
+            ''.join(details['foo_content'].iter_text()),
+            Equals('foo ok'))
+        self.expectThat(
             ''.join(details['traceback'].iter_text()),
             Contains('_setUp broke'))
-        self.assertThat(
+        self.expectThat(
             ''.join(details['traceback-1'].iter_text()),
             Contains('foobar'))
 
