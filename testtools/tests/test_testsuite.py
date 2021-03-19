d528842b99b16efce212e15dae3f0a54927d06d8
Cyril Roelandt <cyril@redhat.com>
2021-03-19 02:50:13 +0000

    Fix tests with Python 3.10
    In Python 3, error messages have become a bit more precise. For
    instance, the following code snippet:
    
    ----
    class Foo():
        def bar(self, a):
            pass
    
    try:
        Foo().bar(1, 2)
    except TypeError as e:
        print(e)
    ----
    
    will return:
    
    - in Python 3.9: "bar() takes 2 positional arguments but 3 were given"
    - in Python 3.10: "Foo.bar() takes 2 positional arguments but 3 were
      given"
    
    Fix our tests accordingly.

--- testtools/tests/test_testsuite.py
+++ testtools/tests/test_testsuite.py
@@ -181,7 +181,7 @@ class TestConcurrentStreamTestSuiteRun(TestCase):
     test.run(process_result)
 """, doctest.ELLIPSIS))
         self.assertThat(events[3][6].decode('utf8'), DocTestMatches("""\
-TypeError: run() takes ...1 ...argument...2...given...
+TypeError: ...run() takes ...1 ...argument...2...given...
 """, doctest.ELLIPSIS))
         events = [event[0:10] + (None,) for event in events]
         events[1] = events[1][:6] + (None,) + events[1][7:]
