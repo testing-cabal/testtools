e75ee0f5cea300cc20c8be764fb4bcf56d96ee97
Jelmer Vernooĳ <jelmer@jelmer.uk>
2023-11-01 20:15:10 +0000

    Also remove TestCommand from setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,7 @@
 #!/usr/bin/env python
 import setuptools
 
-try:
-    import testtools
-    cmd_class = {}
-    if getattr(testtools, 'TestCommand', None) is not None:
-        cmd_class['test'] = testtools.TestCommand
-except:
-    cmd_class = None
-
-
 setuptools.setup(
     python_requires='>=3.6',
-    cmdclass=cmd_class,
     setup_requires=['pbr'],
     pbr=True)
