c0d384cce6e3ff03da7d48e70c4a06d5136bce40
Stephen Finucane <stephen@that.guru>
2026-03-20 17:24:57 +0100

    Add missing ClassVar types
    We also remove MatcherTestProtocol since it is overly complicated
    and not really needed.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- testtools/matchers/test.py
+++ testtools/matchers/test.py
@@ -1,21 +1,9 @@
 # Copyright (c) 2008-2012 testtools developers. See LICENSE for details.
 
 from collections.abc import Callable
-from typing import Any, ClassVar, Protocol, runtime_checkable
+from typing import Any, ClassVar
 
-
-@runtime_checkable
-class MatcherTestProtocol(Protocol):
-    """Protocol for test classes that test matchers."""
-
-    matches_matcher: ClassVar[Any]
-    matches_matches: ClassVar[Any]
-    matches_mismatches: ClassVar[Any]
-    str_examples: ClassVar[Any]
-    describe_examples: ClassVar[Any]
-    assertEqual: Callable[..., Any]
-    assertNotEqual: Callable[..., Any]
-    assertThat: Callable[..., Any]
+from testtools.matchers._impl import Matcher
 
 
 class TestMatchersInterface:
@@ -23,7 +11,17 @@ class TestMatchersInterface:
 
     __test__ = False  # Tell pytest not to collect this as a test class
 
-    def test_matches_match(self: MatcherTestProtocol) -> None:
+    matches_matcher: ClassVar[Matcher[Any]]
+    matches_matches: ClassVar[list[Any]]
+    matches_mismatches: ClassVar[list[Any]]
+    str_examples: ClassVar[Any]
+    describe_examples: ClassVar[Any]
+
+    assertEqual: Callable[..., Any]
+    assertNotEqual: Callable[..., Any]
+    assertThat: Callable[..., Any]
+
+    def test_matches_match(self) -> None:
         matcher = self.matches_matcher
         matches = self.matches_matches
         mismatches = self.matches_mismatches
@@ -34,7 +32,7 @@ class TestMatchersInterface:
             self.assertNotEqual(None, mismatch)
             self.assertNotEqual(None, getattr(mismatch, "describe", None))
 
-    def test__str__(self: MatcherTestProtocol) -> None:
+    def test__str__(self) -> None:
         # [(expected, object to __str__)].
         from testtools.matchers._doctest import DocTestMatches
 
@@ -42,14 +40,14 @@ class TestMatchersInterface:
         for expected, matcher in examples:
             self.assertThat(matcher, DocTestMatches(expected))
 
-    def test_describe_difference(self: MatcherTestProtocol) -> None:
+    def test_describe_difference(self) -> None:
         # [(expected, matchee, matcher), ...]
         examples = self.describe_examples
         for difference, matchee, matcher in examples:
             mismatch = matcher.match(matchee)
             self.assertEqual(difference, mismatch.describe())
 
-    def test_mismatch_details(self: MatcherTestProtocol) -> None:
+    def test_mismatch_details(self) -> None:
         # The mismatch object must provide get_details, which must return a
         # dictionary mapping names to Content objects.
         examples = self.describe_examples
