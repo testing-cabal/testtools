7c79476208d429ff706ab32133058b0dd04e72dc
Jelmer Vernooĳ <jelmer@jelmer.uk>
2026-04-24 01:08:38 +0100

    Don't leak testtools internals into stdlib TestResult error reports

--- testtools/_types.py
+++ testtools/_types.py
@@ -4,5 +4,5 @@ from types import TracebackType
 from typing import TypeAlias
 
 # Type for exc_info tuples from sys.exc_info()
-ExcInfo: TypeAlias = tuple[type[BaseException], BaseException, TracebackType]
+ExcInfo: TypeAlias = tuple[type[BaseException], BaseException, TracebackType | None]
 OptExcInfo: TypeAlias = ExcInfo | tuple[None, None, None]
